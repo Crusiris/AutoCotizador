80521c9c89bba9c6072698a88732546aa83efdc2
crusiris <cruzsirys@gmail.com>
2020-04-16 19:26:53 -0400

    Estilos para el formulario, usando componentes de estilo a traves de la libreria emotion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import styled from '@emotion/styled'
 import Header from './components/Header'
+import Formulario from './components/Formulario'
+
+  //Estilos css, usando la libreria EMOTION STYLED
+  const Container = styled.div`
+  max-width:600px;
+  margin: 0 auto;
+  `;
+
+  const ContainerForm = styled.div`
+  background-color:#FFF;
+  padding:3rem;
+  `;
 
 
 function App() {
-  return (
-    
-    <Header title="Cotizador de automoviles"/>
-  );
+    return (
+        <Container>
+          <Header title="Cotizador de automoviles"/>
+
+          <ContainerForm>
+          <Formulario/>
+          </ContainerForm>
+
+        </Container>
+        
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const Header = ({title}) => (
-    <h1>{title}</h1>
-)
+  //Estilos css, usando la libreria EMOTION STYLED
+    const ContainerHeader = styled.header `
+    background-color:#26C6DA;
+    padding:10px;
+    font-weight:bold;
+    color:#FFFFFF;
+    margin:1em;
+    `;
+
+    const TextoHeader = styled.h1`
+    font-size:2rem;
+    margin:0;
+    font-family: 'Slabo 27px', serif;
+    text-align:center;
+    `;
+
+const Header = ({title}) => {
+
+    return(
+        <ContainerHeader>
+            <TextoHeader>{title}</TextoHeader>
+        </ContainerHeader>
+    )
+        
+}
+   
  
 export default Header;
